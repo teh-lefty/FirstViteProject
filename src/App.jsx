19a607dcd2ebf6b9b3496d12eaa62b95c4fdf8a8
teh-lefty <102568631+teh-lefty@users.noreply.github.com>
2024-01-18 23:54:03 -0800

    featured pup added

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,31 @@ import { useState } from 'react'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
   console.log("puppyList: ", puppyList)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
 
   return (
     
      <div className="App">
       {
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
         })
       }
+      
+        
+        { featPupId && (
+          <div className="featPupID">
+        <h2>Featured Puppy:</h2> 
+          <h1>{featuredPup.name}</h1>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>
+      )}
         
       </div>
     
